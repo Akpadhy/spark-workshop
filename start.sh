b8c05490101470f34ead338443d64ff636f77655
Dean Wampler <dean@concurrentthought.com>
2014-12-17 16:18:41 -0600

    More command line options.

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 help() {
-  echo "usage: $0 [-h|--help] [ui|shell|sbt]"
-  echo "where: ui is the default and sbt is an alias for shell."
-  echo "Use ui for the web-based UI. Use shell/sbt for the command line."
+  cat <<-EOF
+usage: $0 [-h|--help] [--mem N] [-nq|--not-quiet] [ui|shell|sbt]
+where:
+  ui         Use the web UI (default).
+  shell|sbt  Use the command-line shell (SBT) interface.
+  --mem N    The default memory for Activator is 4096 (MB), which is also used
+             to run the non-Hadoop Spark examples. Use a larger integer value
+             N if you experience out of memory errors.
+  -nq|--not-quiet
+             By default, most Activator output is suppressed when the web UI is
+             used. For debugging purposes when running the web UI, use this
+             option to show this output.
+EOF
 }
 
 filter_garbage() {
+  quiet=$1
   while read line
   do
     if [[ ${line} =~ Please.point.your.browser.at ]] ; then
@@ -18,24 +29,47 @@ filter_garbage() {
       echo "=================================================================="
     elif [[ ${line} =~ play.-.Application.started ]] ; then
       echo $line
+    elif [[ -z $quiet ]] ; then
+      echo $line
     fi
   done
 }
 
-case $1 in
-  ui|shell) mode=$1    ;;
-  sbt)      mode=shell; echo "Using shell mode" ;;
-  "")       mode=ui    ;;
-  -h|--h*)
-    help
-    exit 0
-    ;;
-  *)
-    echo "Unrecognized argument $1"
-    help
-    exit 1
-    ;;
-esac
+java_opts() {
+  mem=$1
+  perm=$(( $mem / 4 ))
+  (( $perm < 512 )) && perm=512
+  echo "-Xms${mem}M -Xmx${mem}M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=${perm}M"
+}
+
+mem=4096
+quiet=yes
+mode=ui
+while [ $# -gt 0 ]
+do
+  case $1 in
+    ui|shell) mode=$1    ;;
+    sbt)      mode=shell; echo "Using shell mode" ;;
+    "")       mode=ui    ;;
+    -h|--h*)
+      help
+      exit 0
+      ;;
+    --mem)
+      shift
+      mem=$1
+      ;;
+    -nq|--not-quiet)
+      quiet=
+      ;;
+    *)
+      echo "Unrecognized argument $1"
+      help
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 dir=$(dirname $0)
 ip=$($dir/scripts/getip.sh)
@@ -55,11 +89,16 @@ then
 fi
 
 # Invoke with NOOP=x start.sh to suppress execution:
-echo $HOME/activator/activator -Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode
+ARGS=(-Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode)
+JAVA_OPTS=$(java_opts $mem)
+echo JAVA_OPTS=\"$JAVA_OPTS\" $HOME/activator/activator ${ARGS[@]}
+log="$dir/activator.log"
 [[ -z $NOOP ]] || exit 0
 if [[ $mode = ui ]]
 then
-  $HOME/activator/activator -Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode 2>&1 | tee "$dir/activator.log" | filter_garbage
+  echo "Running the Web UI. Writing all activity to $log"
+  JAVA_OPTS="$JAVA_OPTS" $HOME/activator/activator ${ARGS[@]} 2>&1 | tee "$log" | filter_garbage $quiet
 else
-  $HOME/activator/activator -Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode
+  echo "Running the command shell. Writing all activity to $log"
+  JAVA_OPTS="$JAVA_OPTS" $HOME/activator/activator ${ARGS[@]} 2>&1 | tee "$log"
 fi
